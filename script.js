dacbd56a9e9044c939d1ed174a080982aa978a38
Brooklyn1985 <98481912+Brooklyn1985@users.noreply.github.com>
2023-01-01 13:34:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 <script>
   function toggleMenu() {
-    var menu = document.getElementById("menu");
+    var menu = document.getElementById("MENU");
     if (menu.style.display === "block") {
       menu.style.display = "none";
     } else {
